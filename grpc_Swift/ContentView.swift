461cc61566a928593367a1edb94aba96d3cf5273
Gopal Rao Gurram <gopal@getepic.com>
2020-10-15 02:31:48 -0400

    Initial Commit

---
+++ grpc_Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  grpc_Swift
+//
+//  Created by Gopal Rao Gurram on 10/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
